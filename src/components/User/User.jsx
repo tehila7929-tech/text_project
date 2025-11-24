8d9cd8ab60f40bef1f0e748e57be58c02b415f65
MyName <me@example.com>
2025-11-25 01:11:13 +0200

    יוזר

--- src/components/User/User.jsx
+++ src/components/User/User.jsx
@@ -1,11 +1,12 @@
 import { useState } from 'react'
 import DocumentEditor from "../DocumentEditor/DocumentEditor"
 import Button from '../Button'
+import LogInAndSignUp from './LogInAndSignUp'
 
 function User(props) {
     return (
-        <>
-            <DocumentEditor screen={props.screen} setScreen={props.setScreen} />
+        <>{!(props.userLoggedIn) && <LogInAndSignUp setUserLoggedIn={props.setUserLoggedIn}></LogInAndSignUp>}
+            {props.userLoggedIn&&<DocumentEditor screen={props.screen} setScreen={props.setScreen} />}
         </>
     )
 }
