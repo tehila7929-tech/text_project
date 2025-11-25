6e844a4a5c93788526a9d80dee960dfa90bc0ac9
Tehila <your_email@example.com>
2025-11-25 04:14:20 +0200

    ms. carbolita

--- src/components/DocumentEditor/Screen/Screen.jsx
+++ src/components/DocumentEditor/Screen/Screen.jsx
@@ -4,14 +4,15 @@ import './Screen.css'
 
 export default function Screen(props) {
     return (
-        <div id='screen'>
-            <div> {props.thisFile.screen.map((element, index) => {
+        < >
+            <p>{props.thisFile.name}</p>
+            <div id='screen'> {props.thisFile.screen.map((element, index) => {
                 const isHebrew = element.ch.match(/[\u0590-\u05FF]/);
                 const textStyle = isHebrew ? { direction: 'rtl', display: 'inline' } : { direction: 'ltr', display: 'inline' };
                 return (element.ch === "/n" ? <br key={index} ></br> :
                     <span key={index} style={{ ...element.style, ...textStyle }}>  {element.ch === ' ' ? <span>&nbsp;</span> : element.ch} </span>);
             })}</div>
-        </div>
+        </>
     );
 }
 
