e019e3ba4f3fc97905f5fcfd10ab7350f455b4f8
Tehila <your_email@example.com>
2025-11-25 15:15:55 +0200

    aaa

--- src/components/DocumentEditor/Screen/Screen.jsx
+++ src/components/DocumentEditor/Screen/Screen.jsx
@@ -7,12 +7,15 @@ export default function Screen(props) {
         < >
             <p>{props.thisFile.name}</p>
             {props.workingThisDocument ?
-                <div id='screen'> {props.screen.map((element, index) => {
-                    const isHebrew = element.ch.match(/[\u0590-\u05FF]/);
-                    const textStyle = isHebrew ? { direction: 'rtl', display: 'inline' } : { direction: 'ltr', display: 'inline' };
-                    return (element.ch === "/n" ? <br key={index} ></br> :
-                        <span key={index} style={{ ...element.style, ...textStyle }}>  {element.ch === ' ' ? <span>&nbsp;</span> : element.ch} </span>);
-                })}</div>
+                <div id='screen'>
+                    {props.screen.map((element, index) => {
+                        const isHebrew = element.ch.match(/[\u0590-\u05FF]/);
+                        const textStyle = isHebrew ? { direction: 'rtl', display: 'inline' } : { direction: 'ltr', display: 'inline' };
+                        return (element.ch === "/n" ? <br key={index} ></br> :
+                            <span key={index} style={{ ...element.style, ...textStyle }}>  {element.ch === ' ' ? <span>&nbsp;</span> : element.ch} </span>);
+                    })}
+                    <span>✏️</span>
+                </div>
                 : <div id='screen'> {props.thisFile.screen.map((element, index) => {
                     const isHebrew = element.ch.match(/[\u0590-\u05FF]/);
                     const textStyle = isHebrew ? { direction: 'rtl', display: 'inline' } : { direction: 'ltr', display: 'inline' };
